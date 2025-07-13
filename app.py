58cdbb919584c1fd7f4d2a74bee0d152fa5f2bb6
Kanishka Anand <kanishkaadz@gmail.com>
2025-07-13 16:52:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def input_pdf_setup(uploaded_file):
 
 ## Streamlit App
 
-st.set_page_config(page_title="OptiResume - ATS Resume Expert", page_icon=":guardsman:", layout="wide")
+st.set_page_config(page_title="OptiResume - ATS Resume Expert", page_icon="logo.png", layout="wide")
 st.header("OptiResume - ATS Resume Expert")
 input_text=st.text_area("Job Description: ",key="input")
 uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
